6ed78dbffb2661c62329927e33ec3d69fe5d442c
Gustavo-Bertti <bertinhobertti@hotmail.com>
2024-04-16 20:58:13 -0300

    AjusteCrud

--- src/main/java/br/com/fiap/smartcash/controller/FluxoDeCaixaController.java
+++ src/main/java/br/com/fiap/smartcash/controller/FluxoDeCaixaController.java
@@ -3,8 +3,7 @@ package br.com.fiap.smartcash.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,13 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
-
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
 import br.com.fiap.smartcash.model.FluxoDeCaixa;
 import br.com.fiap.smartcash.repository.FluxoDeCaixaRepository;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -36,27 +34,25 @@ public class FluxoDeCaixaController {
     FluxoDeCaixaRepository repository;
 
     @GetMapping
-    public List<FluxoDeCaixa> getAll() {
+    public List<FluxoDeCaixa> readAll() {
         return repository.findAll();
     }
 
     @PostMapping
     @ResponseStatus(CREATED)
-    public FluxoDeCaixa create(@RequestBody FluxoDeCaixa fluxoDeCaixa) {
-        log.info("cadastrando fluxo de caixa: {},", fluxoDeCaixa);
+    public FluxoDeCaixa create(@RequestBody @Valid FluxoDeCaixa fluxoDeCaixa) {
         return repository.save(fluxoDeCaixa);
     }
 
     @GetMapping(path = "{id}")
-    public ResponseEntity<FluxoDeCaixa> get(@PathVariable Long id) {
-        log.info("buscando fluxo de caixa com id {}", id);
+    public ResponseEntity<FluxoDeCaixa> readItem(@PathVariable Long id) {
 
         return repository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping(path = "{id}")
     public FluxoDeCaixa update(@PathVariable Long id, @RequestBody FluxoDeCaixa fluxoDeCaixa) {
-        log.info("buscando fluxo de caixa com id {}", id);
+        
         verificarSeExisteCategoria(id);
         fluxoDeCaixa.setId(id);
         repository.save(fluxoDeCaixa);
@@ -66,7 +62,7 @@ public class FluxoDeCaixaController {
     @DeleteMapping(path = "{id}")
     @ResponseStatus(NO_CONTENT)
     public void remove(@PathVariable Long id) {
-        log.info("buscando fluxo de caixa com id {}", id);
+        
         verificarSeExisteCategoria(id);
         repository.deleteById(id);
        
--- src/main/java/br/com/fiap/smartcash/model/FluxoDeCaixa.java
+++ src/main/java/br/com/fiap/smartcash/model/FluxoDeCaixa.java
@@ -1,9 +1,15 @@
 package br.com.fiap.smartcash.model;
 
+import java.time.LocalDate;
+
+import br.com.fiap.smartcash.validation.Tipo;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,9 +18,16 @@ public class FluxoDeCaixa{
 
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Tipo
     private String tipo;
+
+    @Positive
     private double valor;
+
+    @NotBlank @Size(min = 3, max = 250)
     private String descricao;
-    private String data;
+    
+    private LocalDate data_inclusao = LocalDate.now();
     
 }  
